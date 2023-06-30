d547b2d961b7fd9514640b26a26ea51046a6ebeb
Piotr Hajdukiewicz <p.hajdukiewicz@emporix.com>
2023-06-30 16:23:54 +0200

    sharedQuotaPolicy fixes in js

--- shared_flows/sharedQuotaPolicy/sharedflowbundle/resources/jsc/setQuotaWarningLevel.js
+++ shared_flows/sharedQuotaPolicy/sharedflowbundle/resources/jsc/setQuotaWarningLevel.js
@@ -1,12 +1,12 @@
 var quotaCalloutResponse = context.getVariable("quotaCalloutResponse.content");
 var quotaCalloutResponseObject = JSON.parse(quotaCalloutResponse);
 
-var quotaAllowed = quotaCalloutResponseObject.quota-allowed-count;
-var quotaAvailable = quotaCalloutResponseObject.quota-available-count;
+var quotaAllowed = quotaCalloutResponseObject.quota_allowed_count;
+var quotaAvailable = quotaCalloutResponseObject.quota_available_count;
 
 var quotaPercentageUsed = quotaAvailable/quotaAllowed * 100;
 
-print(quotaPercentageUsed)
+print("Quota percentage used: " + quotaPercentageUsed)
 
 if (quotaPercentageUsed === 50 ) {
     context.setVariable("quota_percent_reached", "50");
