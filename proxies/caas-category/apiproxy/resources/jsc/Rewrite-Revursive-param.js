5653798939f89203cf0b88b7890b04ae8ff3f492
Piotr Hajdukiewicz <p.hajdukiewicz@emporix.com>
2023-11-07 13:07:58 +0100

    update caas-category proxy (remove unused policies and jsc files)

--- proxies/caas-category/apiproxy/resources/jsc/Rewrite-Revursive-param.js
+++
@@ -1,6 +0,0 @@
-var recursiveParam = context.getVariable('request.queryparam.recursive')
-var withSubcategoriesParam = context.getVariable('request.queryparam.withSubcategories')
-context.removeVariable("request.queryparam.recursive")
-if(recursiveParam !== null && withSubcategoriesParam === null){
-    context.setVariable("request.queryparam.withSubcategories", recursiveParam)
-}
--- proxies/caas-category/apiproxy/resources/jsc/rewritePathSuffix.js
+++
@@ -1,5 +0,0 @@
-if (context.flow == "TARGET_REQ_FLOW") {
-    var tenant = context.getVariable('request.header.hybris-tenant')
-    var url = "https://category-tree.k8s-stage.emporix.io/"
-    context.setVariable("target.url", url)
-}
