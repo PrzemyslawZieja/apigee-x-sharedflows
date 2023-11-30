51239b1e62fac201bd95824c41bfe23ad2fc28d1
Piotr Hajdukiewicz <p.hajdukiewicz@emporix.com>
2023-11-30 14:26:27 +0100

    update caas-customer - call oauth token using proper url

--- proxies/caas-customer/apiproxy/resources/jsc/parseConfigurationEnvironment.js
+++ proxies/caas-customer/apiproxy/resources/jsc/parseConfigurationEnvironment.js
@@ -3,12 +3,15 @@ var env = context.getVariable("environment.name");
 if(env.match(/.*prod.*/)){
     context.setVariable("SocialLogin.k8sEnvironment", "prod");
     context.setVariable("SocialLogin.apigeeEnvironment", "prod");
+    context.setVariable("SocialLogin.tokenCalloutUrl", "api.emporix.io");
 }else if (env.match(/.*stage.*/)){
     context.setVariable("SocialLogin.k8sEnvironment", "stage");
     context.setVariable("SocialLogin.apigeeEnvironment", "stage");
+    context.setVariable("SocialLogin.tokenCalloutUrl", "api-stage.emporix.io");
 }else{
     context.setVariable("SocialLogin.k8sEnvironment", "develop");
     context.setVariable("SocialLogin.apigeeEnvironment", "dev");
+    context.setVariable("SocialLogin.tokenCalloutUrl", "api-develop.emporix.io");
 }
 
 context.setVariable("SocialLogin.scopes", "tenant=" + tenant);
