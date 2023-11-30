c196d7fdcd729ce0def7663a3373d6015163ad72
Piotr Hajdukiewicz <p.hajdukiewicz@emporix.com>
2023-11-30 09:56:00 +0100

    update caas-customer - sync changes from edge

--- proxies/caas-customer/apiproxy/resources/jsc/parseConfigurationEnvironment.js
+++ proxies/caas-customer/apiproxy/resources/jsc/parseConfigurationEnvironment.js
@@ -1,14 +1,15 @@
 var tenant = context.getVariable("SocialLogin.tenant");
 var env = context.getVariable("environment.name");
-if(env == "prod"){
+if(env.match(/.*prod.*/)){
     context.setVariable("SocialLogin.k8sEnvironment", "prod");
     context.setVariable("SocialLogin.apigeeEnvironment", "prod");
-}else if(env == "stage"){
+}else if (env.match(/.*stage.*/)){
     context.setVariable("SocialLogin.k8sEnvironment", "stage");
     context.setVariable("SocialLogin.apigeeEnvironment", "stage");
-} else {
+}else{
     context.setVariable("SocialLogin.k8sEnvironment", "develop");
     context.setVariable("SocialLogin.apigeeEnvironment", "dev");
 }
+
 context.setVariable("SocialLogin.scopes", "tenant=" + tenant);
 print(context.getVariable('SocialLogin.scopes'))
